2aaf9eb347fc8a750fc61e46ddacacbd72acfb7b
nahueldev <nabudev01@gmail.com>
2024-09-05 15:19:11 -0300

    prueba de la api

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ app.use(bodyParser.json());
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
 })
 .then(() => console.log('Conectado a MongoDB'))
 .catch((error) => console.log('Error conectando a MongoDB:', error));
